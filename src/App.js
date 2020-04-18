3492e3ac3969c50bee3da4859d1dac4300d9e2ff
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-18 01:04:32 -0500

    refactor(app.js): Se configura App.js para nueva aplicación

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Header from "./components/Header.js";
+import GifList from "./components/GIfList.js";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	const [keyWord, setkeyWord] = useState("morty");
+	return (
+		<div className="App">
+			<Header handleOutput={setkeyWord} />
+			<div className="App-wrapper">
+				<GifList keyWord={keyWord} />
+			</div>
+		</div>
+	);
 }
-
-export default App;
