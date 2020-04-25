6cf2d1bf68c530deb30b95a301af93b71a30f512
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-25 10:55:23 -0500

    feat(service/getGifsByKeyWord.js): new file

---
+++ src/service/getGifsByKeyWord.js
@@ -0,0 +1,22 @@
+import {
+	DEFAULT_SEARCH_TERM,
+	DEFAULT_SEARCH_LIMIT,
+	DEFAULT_SEARCH_OFFSET,
+	URL_GIFs_KEYWORD,
+} from "../shared/index.js";
+
+export async function getGifsByKeyWord({
+	keyWord = DEFAULT_SEARCH_TERM,
+	limit = DEFAULT_SEARCH_LIMIT,
+	offset = DEFAULT_SEARCH_OFFSET,
+}) {
+	const URLAPI = URL_GIFs_KEYWORD({ keyWord, limit, offset });
+	const FetchAPI = await fetch(URLAPI);
+	const resAPI = await FetchAPI.json();
+	const { data = [] } = resAPI;
+	return data.map((image) => ({
+		url: image.images.preview_gif.url,
+		title: image.title,
+		id: image.id,
+	}));
+}
