1604b8645d20dbfbcd9f78f5e4cfa6da6fcab3ce
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-25 10:42:54 -0500

    feat(Limit.{js,css}): new component

---
+++ src/components/Limit.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Limit.css";
+import { LIMITS, LIMIT_MEDIUM } from "../shared/index.js";
+
+export default function LimitGifs({ outputLimit = () => {} }) {
+	function emitChange(e) {
+		outputLimit(e.target.value);
+	}
+	return (
+		<label className="gif-filter_field" htmlFor="selectLimit">
+			<select
+				onChange={emitChange}
+				defaultValue={LIMIT_MEDIUM}
+				id="selectLimit"
+			>
+				{LIMITS.map((limit) => (
+					<option value={limit} key={limit}>
+						{limit}
+					</option>
+				))}
+			</select>
+		</label>
+	);
+}
