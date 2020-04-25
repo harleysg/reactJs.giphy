f4d0967e9c38716e4cf8ca5379811a2fe256d47c
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-25 10:54:17 -0500

    feat(shared/index.js): new file
    Constant var and functions

---
+++ src/shared/index.js
@@ -0,0 +1,30 @@
+// Giphy API Key - Get yours at https://developers.giphy.com/dashboard/
+export const API_KEY = "PZU49dYdw6JjhmwHgp9qljnChyuRF8hV";
+
+// URL for giphy search API
+export const API_URL = "https://api.giphy.com/v1/gifs/";
+export const API_SEARCH_URL = `${API_URL}search`;
+export const API_ID_URL = API_URL;
+
+// Initial search values
+export const DEFAULT_SEARCH_TERM = "cats";
+export const REGEX_TERM = "([a-zA-Z0-9]{2,})";
+export const DEFAULT_SEARCH_LIMIT = 25;
+export const DEFAULT_SEARCH_OFFSET = 0;
+
+// Results limits
+export const LIMIT_LOW = 5;
+export const LIMIT_MEDIUM = 25;
+export const LIMIT_HIGH = 100;
+export const LIMITS = [LIMIT_LOW, LIMIT_MEDIUM, LIMIT_HIGH];
+
+export function URL_GIFs_KEYWORD({
+	keyWord = DEFAULT_SEARCH_TERM,
+	limit = LIMIT_MEDIUM,
+	offset = DEFAULT_SEARCH_OFFSET,
+}) {
+	return `${API_SEARCH_URL}?api_key=${API_KEY}&q=${keyWord}&limit=${limit}&offset=${offset}&rating=G&lang=en`;
+}
+export function URL_GIFs_ID({ ID }) {
+	return `${API_ID_URL}${ID}?api_key=${API_KEY}`;
+}
