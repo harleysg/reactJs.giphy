17bbcd702e4595b2562a9c57d3046c35c568150c
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-04-25 10:58:33 -0500

    refactor(Gif.js): se usa Link from wouter

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,24 +1,27 @@
 import React from "react";
+import { Link } from "wouter";
 import "./Gif.css";
 
 export function Gif({ title, id, url }) {
 	return (
-		<article className="gif-card">
-			<header className="gif-card_description">
-				<h4 className="gif-card_title">{title}</h4>
-				<h5 className="gif-card_title">
-					<small>{id}</small>
-				</h5>
-			</header>
-			<figure className="gif-card_figure">
-				<img
-					className="gif-card_image"
-					src={url}
-					id={id}
-					alt={title}
-					key={id}
-				/>
-			</figure>
-		</article>
+		<Link to={id}>
+			<a href className="gif-card">
+				<header className="gif-card_description">
+					<h4 className="gif-card_title">{title}</h4>
+					<h5 className="gif-card_title">
+						<small>{id}</small>
+					</h5>
+				</header>
+				<figure className="gif-card_figure">
+					<img
+						className="gif-card_image"
+						src={url}
+						id={id}
+						alt={title}
+						key={id}
+					/>
+				</figure>
+			</a>
+		</Link>
 	);
 }
