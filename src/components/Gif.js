200b240aab27aefc517566d089028962c407cf1f
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-05-02 00:46:36 -0500

    refactor(Gif.js): ajuste router Link to

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -2,9 +2,9 @@ import React from "react";
 import { Link } from "wouter";
 import "./Gif.css";
 
-export function Gif({ title, id, url }) {
+export default function Gif({ title, id, url }) {
 	return (
-		<Link to={id}>
+		<Link to={`/gif/${id}`}>
 			<a href className="gif-card">
 				<header className="gif-card_description">
 					<h4 className="gif-card_title">{title}</h4>
