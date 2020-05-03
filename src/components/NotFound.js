7bcf3cd38d5ff69b33db5dc77df50257ac64107f
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-05-03 16:01:52 -0500

    refactor(NotFound.js): csModules  + new design
    Se implementa el uso de cssModules, cambio de nombre a archivo css.
    Se implementa nuevo diseño de pagina

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,20 +1,27 @@
 import React from "react";
 
-import { Link } from "wouter";
-
-import Header from "./Header.js";
+import css from "./NotFound.module.css";
 
 export default function NotFound() {
 	return (
 		<>
-			<Header>
-				<div className="App-header_block">
-					<Link to="/" className="c-link">
-						Go to Home
-					</Link>
+			<main className={css["c-notFound_app"]}>
+				<div className={css["c-notFound_page"]}>
+					<div className={css["c-notFound_content"]}>
+						<h1>404</h1>
+						<h2>Page not found</h2>
+						<p>I tried to catch some fog, but i mist</p>
+						<button onClick={() => window.history.back()}>
+							back to home
+						</button>
+					</div>
+					<img
+						className={css["c-notFound_img"]}
+						src="http://www.supah.it/dribbble/008/008.jpg"
+						alt="demo"
+					/>
 				</div>
-			</Header>
-			<div className="App-wrapper">404, not found!</div>
+			</main>
 		</>
 	);
 }
