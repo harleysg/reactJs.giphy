3241cb4ef568166191946f20bacb31ad584787e3
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-05-03 15:54:20 -0500

    feat(Gif.js): cambio a cssModules

--- src/components/Gif.js
+++ src/components/Gif.js
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "wouter";
-import "./Gif.css";
+import css from "./Gif.module.css";
 
 export default function Gif({ title, id, url }) {
 	return (
 		<Link to={`/gif/${id}`}>
-			<a href className="gif-card">
-				<header className="gif-card_description">
-					<h4 className="gif-card_title">{title}</h4>
-					<h5 className="gif-card_title">
+			<a href className={css["gif-card"]}>
+				<header className={css["gif-card_description"]}>
+					<h4 className={css["gif-card_title"]}>{title}</h4>
+					<h5 className={css["gif-card_title"]}>
 						<small>{id}</small>
 					</h5>
 				</header>
-				<figure className="gif-card_figure">
+				<figure className={css["gif-card_figure"]}>
 					<img
-						className="gif-card_image"
+						className={css["gif-card_image"]}
 						src={url}
 						id={id}
 						alt={title}
