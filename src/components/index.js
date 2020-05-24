97711155eefbbcc5bccd3effa9b52647aedecd17
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-05-23 22:40:07 -0500

    feat(components/index): new file manejador index de componentes

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+import Header from "./Header.js";
+import Categories from "./Categories";
+import Gif from "./Gif";
+import GifList from "./GifList";
+import Spinner from "./Spinner";
+import Limit from "./Limit";
+import NotFound from "./NotFound";
+import Search from "./Search";
+
+export { Header, Categories, Gif, GifList, Limit, NotFound, Search, Spinner };
