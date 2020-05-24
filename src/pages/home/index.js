b89d52a082dff03310fba1ae2d995459fdd87414
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-05-24 00:33:52 -0500

    refactor(page/home): Se agrega component GifList, Spinner y hook useGifs

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -3,14 +3,19 @@ import { useLocation } from "wouter";
 
 import { DEFAULT_SEARCH_TERM } from "shared/index.js";
 
-import { Header, Search } from "components/index";
+import { Header, Search, GifList, Spinner } from "components/index";
+import TrendingSearches from "components/TrendingSearches/index";
 
+import { useGifs } from "hooks/index.js";
 import Context from "context/app.context";
+
+import css from "./Home.module.css";
 
 export default function GifHomePage() {
 	const { name } = useContext(Context);
 	const [, setkeyWord] = useState(DEFAULT_SEARCH_TERM);
 	const [, pushLocation] = useLocation();
+	const { loading, gifs } = useGifs();
 
 	function handleFilterChange(value) {
 		setkeyWord(value);
@@ -32,7 +37,16 @@ export default function GifHomePage() {
 					<Search handleOutput={handleFilterChange} />
 				</div>
 			</Header>
-			<div className="App-home"></div>
+			<div className="App-home">
+				<div className={`App-wrapper ${css["App-home_wrapper"]}`}>
+					<div className={css["sectionScroller"]}>
+						<TrendingSearches />
+					</div>
+					<aside className={css["aside"]}>
+						{loading ? <Spinner /> : <GifList gifs={gifs} />}
+					</aside>
+				</div>
+			</div>
 		</>
 	);
 }
