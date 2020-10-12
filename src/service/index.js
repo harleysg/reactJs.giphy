d7962f09059dd455a2ac266829bf2adb3d30420f
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-10-12 18:28:44 -0500

    refactor(serices/index): se agregan nuevos servicios
    browserStorage.js
    loginServices.js
    parseJWT.js

--- src/service/index.js
+++ src/service/index.js
@@ -1,5 +1,8 @@
+import { addFavService } from "service/addFavService";
 import { getGifsByKeyWord } from "./getGifsByKeyWord.js";
 import { getSingleGifById } from "./getSingleGifById.js";
 import getTrendingsTerms from "./getTrendingsTerms";
+import { loginService } from "service/loginService";
+import { parseJwt } from "service/parseJwt";
 
-export { getGifsByKeyWord, getSingleGifById, getTrendingsTerms };
+export { addFavService, getGifsByKeyWord, getSingleGifById, getTrendingsTerms, loginService, parseJwt };
--- src/service/loginService.js
+++ src/service/loginService.js
@@ -1,25 +1,27 @@
 export function loginService({ username, password }) {
-    console.log(`loginService -> { username, password }`, { username, password });
-    
-    // Fake login API
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF0dXMiOiJvayIsIm5hbWUiOiJIYXJsZXkgU2FudG9zIEdhcnrDs24iLCJ1c2VybmFtZSI6ImdhbG8ifQ._p2OxuLuz-LQ8WERn9eEHE6iojyKXVJzcW3MoGcXjYk")
-        }, 2500)
-    })
-    
+  console.log(`loginService -> { username, password }`, { username, password });
 
-    /**
-     * Login API
-     * 
-     * return fetch(URL,{
-     * method: "POST",
-     * headers: { "Content-type": "application/json" },
-     * body: JSON.stringify({username, password})
-     * }).then(res => {
-     *  !res.ok thrwo new Error("Error: Respuesta erronea al loguin")
-     *    return res.json()
-     * }).then(jwt => jwt)
-     * 
-    */
+  // Fake login API
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        jwt:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdGF0dXMiOiJvayIsIm5hbWUiOiJIYXJsZXkgU2FudG9zIEdhcnrDs24iLCJ1c2VybmFtZSI6ImdhbG8ifQ._p2OxuLuz-LQ8WERn9eEHE6iojyKXVJzcW3MoGcXjYk",
+      });
+    }, 2500);
+  });
+
+  /**
+   * Login API
+   *
+   * return fetch(URL,{
+   * method: "POST",
+   * headers: { "Content-type": "application/json" },
+   * body: JSON.stringify({username, password})
+   * }).then(res => {
+   *  !res.ok thrwo new Error("Error: Respuesta erronea al loguin")
+   *    return res.json()
+   * }).then(jwt => jwt)
+   *
+   */
 }
