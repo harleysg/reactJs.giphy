24dc4bd2970657fe0021ce476697f1ba60abd8b6
Harley Santos Garzon <harley.santos.garzon@gmail.com>
2020-10-12 18:27:35 -0500

    style(home/index.js): prettier changes

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,35 +9,33 @@ import Context from "context/app.context";
 import css from "./Home.module.css";
 
 export default function GifHomePage() {
-	const { name } = useContext(Context);
-	const { loading, gifs } = useGifs();
-	
-	return (
-		<>
-			<Head>
-				<Title>{loading ? "loading..." : name}</Title>
-				<Meta name="description" content={`welcome to ${name}`} />
-			</Head>
-			<Header>
-				<div className="App-header_block">
-					<div className="o-brand">
-						<Brand/>
-					</div>
-				</div>
-				<div className="App-header_block">
-					<Search />
-				</div>
-			</Header>
-			<div className="App-home">
-				<div className={`App-wrapper ${css["App-home_wrapper"]}`}>
-					<div className={css["sectionScroller"]}>
-						<TrendingSearches />
-					</div>
-					<aside className={css["aside"]}>
-						{loading ? <Spinner /> : <GifList gifs={gifs} />}
-					</aside>
-				</div>
-			</div>
-		</>
-	);
+  const { name } = useContext(Context);
+  const { loading, gifs } = useGifs();
+
+  return (
+    <>
+      <Head>
+        <Title>{loading ? "loading..." : name}</Title>
+        <Meta name="description" content={`welcome to ${name}`} />
+      </Head>
+      <Header>
+        <div className="App-header_block">
+          <div className="o-brand">
+            <Brand />
+          </div>
+        </div>
+        <div className="App-header_block">
+          <Search />
+        </div>
+      </Header>
+      <div className="App-home">
+        <div className={`App-wrapper ${css["App-home_wrapper"]}`}>
+          <div className={css["sectionScroller"]}>
+            <TrendingSearches />
+          </div>
+          <aside className={css["aside"]}>{loading ? <Spinner /> : <GifList gifs={gifs} />}</aside>
+        </div>
+      </div>
+    </>
+  );
 }
